68529e06c4383d6f2cd05875041aa2cc7ac1491a
Tulio <tuliotrindade99@gmail.com>
2021-04-23 18:36:10 -0300

    atividades ' a 7

---
+++ bloco_4/dia_2/atividades.js
@@ -0,0 +1,61 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//q1 - console.log(numbers);
+/*
+ q2 - let soma=0;
+for(let i=0; i<numbers.length; i+=1){
+    soma=soma+numbers[i];
+}
+console.log(soma);
+*/
+/*
+q3 q4
+let soma=0;
+let media=0;
+for(let i=0; i<numbers.length; i+=1){
+    soma=soma+numbers[i];
+    media=soma/numbers.length
+}
+if(media<=20){
+    console.log("menor ou igual a 20")
+    console.log(media);
+}
+if(media>20){
+    console.log("maior que 20")
+    console.log(media);
+}
+*/
+/*
+q5
+let comparador=0;
+for(let i=0;i<numbers.length;i+=1){
+    if(numbers[i]>comparador){
+        comparador=numbers[i];
+    }
+}
+console.log(comparador);
+*/
+/*
+q6
+let contador=0;
+for(let i=0;i<numbers.length;i+=1){
+    if(numbers[i]%2!=0){
+        contador+=1;
+    }
+}
+if(contador==0){
+    console.log("nenhum valor ímpar encontrado")
+}
+else{
+    console.log(contador);
+}
+*/
+/*
+q7
+let comparador=numbers[1];
+for(let i=0;i<numbers.length;i+=1){
+    if(numbers[i]<comparador){
+        comparador=numbers[i];
+    }
+}
+console.log(comparador);
+*/
